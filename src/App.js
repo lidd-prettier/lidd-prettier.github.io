8ef3c321790fe3165021704261ae2640288d64e2
abdelrahmanali <abdalrahman2587@gmail.com>
2024-06-13 15:26:51 -0400

    Made the website responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
 import prettierLogo from './prettier-logo.png';
@@ -9,129 +9,170 @@ import Home from './Home';
 import Privacy from './Privacy';
 import Subscription from './Subscription';
 import Gallery from './Gallery';
+import burgerIcon from './menu-burger.png'; // Add a burger icon image
 
 function App() {
   const [view, setView] = useState('home'); // State to manage current view
+  const [menuOpen, setMenuOpen] = useState(false); // State to manage menu toggle
+  const menuRef = useRef(null);
+
   const handleContactClick = () => {
     window.location.href = 'mailto:prettier@lidd.com';
   };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target) && !event.target.classList.contains('burger-icon')) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef]);
+
   return (
     <Router>
       <div className="App">
         <header className="header">
-          <img src={prettierLogo} alt="Prettier Logo" onClick={() => setView('home')} className="logo" />
-          <span className="logo-text" onClick={() => setView('home')}>Prettier</span>
-          <nav>
+          <div className="logo-container" onClick={() => setView('home')}>
+            <img src={prettierLogo} alt="Prettier Logo" className="logo" />
+            <span className="logo-text">Prettier</span>
+          </div>
+          <nav ref={menuRef} className={`nav ${menuOpen ? 'open' : ''}`}>
             <Link
               to="/features"
               className={view === 'home' ? 'active' : ''}
-              onClick={() => setView('home')}
+              onClick={() => {
+                setView('home');
+              closeMenu();
+            }}
             >
               Features
             </Link>
             <Link
               to="/subscription"
               className={view === 'subscription' ? 'active' : ''}
-              onClick={() => setView('subscription')}
+              onClick={() => {
+                setView('subscription');
+                closeMenu();
+              }}
             >
               Subscription
             </Link>
             <Link
               to="/privacy"
               className={view === 'privacy' ? 'active' : ''}
-              onClick={() => setView('privacy')}
+              onClick={() => { 
+                setView('privacy');
+                closeMenu();
+              }}
             >
               Privacy
             </Link>
             <Link
               to="/gallery"
               className={view === 'gallery' ? 'active' : ''}
-              onClick={() => setView('gallery')}
+              onClick={() => {
+                setView('gallery');
+                closeMenu();
+              }}
             >
               Gallery
             </Link>
           </nav>
           <button className="contact-button" onClick={handleContactClick}>Contact Us</button>
+          <img src={burgerIcon} alt="Menu" className="burger-icon" onClick={toggleMenu} />
         </header>
 
-          <main className="main">
-            <div className="main-content">
-              {view === 'home' && <Home setView={setView} />}
-              {view === 'privacy' && <Privacy />}
-              {view === 'subscription' && <Subscription />}
-              {view === 'gallery' && <Gallery />}
-            </div>
-          </main>
+        <main className="main">
+          <div className="main-content">
+            {view === 'home' && <Home setView={setView} />}
+            {view === 'privacy' && <Privacy />}
+            {view === 'subscription' && <Subscription />}
+            {view === 'gallery' && <Gallery />}
+          </div>
+        </main>
 
-          <footer className="footer">
-            <div className="footer-left">
-              <div className="footer-logo-container">
-                <img src={prettierLogo} alt="Prettier Logo" className="footer-logo" />
-                <span className="footer-logo-text">Prettier</span>
-              </div>
-              <p className="footer-text">
-                Prettier protects your data.<br />
-                Read more in our <Link
-                  to="/privacy"
-                  className={view === 'privacy' ? 'active' : ''}
-                  onClick={() => setView('privacy')}
-                >
-                  Privacy Policy.
-                </Link>
-              </p>
-              <div className="footer-social-icons">
-                <a href="https://x.com/prettierapp​">
-                  <img src={twitterIcon} alt="Twitter" />
+        <footer className="footer">
+          <div className="footer-left">
+            <div className="footer-logo-container">
+              <img src={prettierLogo} alt="Prettier Logo" className="footer-logo" />
+              <span className="footer-logo-text">Prettier</span>
+            </div>
+            <p className="footer-text">
+              Prettier protects your data.<br />
+              Read more in our <Link
+                to="/privacy"
+                className={view === 'privacy' ? 'active' : ''}
+                onClick={() => setView('privacy')}
+              >
+                Privacy Policy.
+              </Link>
+            </p>
+            <div className="footer-social-icons">
+              <a href="https://x.com/prettierapp​">
+                <img src={twitterIcon} alt="Twitter" />
+              </a>
+              <a href="https://www.instagram.com/prettierapp">
+                <img src={instagramIcon} alt="Instagram" />
+              </a>
+            </div>
+          </div>
+          <div className="footer-center">
+            <div className="footer-links">
+              <Link
+                to="/features"
+                className={view === 'home' ? 'active' : ''}
+                onClick={() => setView('home')}
+              >
+                Features
+              </Link>
+              <Link
+                to="/subscription"
+                className={view === 'subscription' ? 'active' : ''}
+                onClick={() => setView('subscription')}
+              >
+                Subscription
+              </Link>
+              <Link
+                to="/gallery"
+                className={view === 'gallery' ? 'active' : ''}
+                onClick={() => setView('gallery')}
+              >
+                Gallery
+              </Link>
+              <Link
+                to="/privacy"
+                className={view === 'privacy' ? 'active' : ''}
+                onClick={() => setView('privacy')}
+              >
+                Privacy
+              </Link>
+            </div>
+          </div>
+          <div className="footer-right">
+            <h3>Let's Get In-Touch</h3>
+            <p>prettier@lidd.com</p>
+            <div className="footer-download-buttons">
+              <div className="download-buttons">
+                <a href="https://apps.apple.com/ca/app/prettier/id6450973776">
+                  <img src={appStoreBadge} alt="Download on the App Store" />
                 </a>
-                <a href="https://www.instagram.com/prettierapp">
-                  <img src={instagramIcon} alt="Instagram" />
-                </a>
               </div>
             </div>
-            <div className="footer-center">
-              <div className="footer-links">
-                <Link
-                  to="/features"
-                  className={view === 'home' ? 'active' : ''}
-                  onClick={() => setView('home')}
-                >
-                  Features
-                </Link>
-                <Link
-                  to="/subscription"
-                  className={view === 'subscription' ? 'active' : ''}
-                  onClick={() => setView('subscription')}
-                >
-                  Subscription
-                </Link>
-                <Link
-                  to="/gallery"
-                  className={view === 'gallery' ? 'active' : ''}
-                  onClick={() => setView('gallery')}
-                >
-                  Gallery
-                </Link>
-                <Link
-                  to="/privacy"
-                  className={view === 'privacy' ? 'active' : ''}
-                  onClick={() => setView('privacy')}
-                >
-                  Privacy
-                </Link>
-              </div>
-            </div>
-            <div className="footer-right">
-              <h3>Let's Get In-Touch</h3>
-              <p>prettier@lidd.com</p>
-              <div className="footer-download-buttons">
-                <div className="download-buttons">
-                  <a href="https://apps.apple.com/ca/app/prettier/id6450973776">
-                    <img src={appStoreBadge} alt="Download on the App Store" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </footer>
+          </div>
+        </footer>
       </div>
     </Router>
   );
